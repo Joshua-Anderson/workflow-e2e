18166331eb554251ea5629e0c674770f5bba95f1
Joshua Anderson <joshua@deis.com>
2016-06-29 16:32:21 -0700

    fix(.): clean up docker-test-integration shell script

--- docker-test-integration.sh
+++ docker-test-integration.sh
@@ -6,11 +6,11 @@
 BASE_URL="https://storage.googleapis.com/workflow-cli"
 URL="$BASE_URL/deis-latest-linux-amd64"
 
-if [[ $CLI_VERSION -ne "latest" ]]; then
+if [[ "$CLI_VERSION" != "latest" ]]; then
 	URL="$BASE_URL/$CLI_VERSION/deis-$CLI_VERSION-linux-amd64"
 fi
 
 echo "Installing Workflow CLI version $CLI_VERSION"
-curl $URL -o /usr/local/bin/deis && chmod +x /usr/local/bin/deis
+curl "$URL" -o /usr/local/bin/deis && chmod +x /usr/local/bin/deis
 
 make test-integration
